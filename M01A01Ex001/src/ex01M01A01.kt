037fa623daf213323be61fd099883a2b9754ff33
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-08 16:54:27 +0000

    M01A01 – Exercício - Módulo 1 Kotlin

--- M01A01Ex001/src/ex01M01A01.kt
+++ M01A01Ex001/src/ex01M01A01.kt
@@ -1,17 +1,18 @@
 fun main(args: Array<String>) {
+/* EXERCICIO M01A01
+    Kotlin é
+    "SUPER"
+    Fácil!
+ */
 
-    println("\n")
+    println("")
     print("Kotlin é\n\"SUPER\"\nFácil!")
     println("\n")
 
+    /* Outra Solução
     println("Kotlin é")
     println("\"SUPER\"")
     println("Fácil!")
     println("\n")
-
-    /*
-    Kotlin é
-    "SUPER"
-    Fácil!
-    * */
+    */
 }
