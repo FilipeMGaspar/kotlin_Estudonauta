31160fb8e38c91edb08f224ce709be69c6440d35
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-08 16:50:14 +0000

    M01A01 – Exercício  - Módulo 1 Kotlin

--- M01A01Ex001/src/ex01M01A01.kt
+++ M01A01Ex001/src/ex01M01A01.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
 
+    println("\n")
     print("Kotlin é\n\"SUPER\"\nFácil!")
+    println("\n")
+
+    println("Kotlin é")
+    println("")
 
     /*
     Kotlin é
