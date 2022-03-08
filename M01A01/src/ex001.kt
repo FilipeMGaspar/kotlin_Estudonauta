9dfc94775c3836d9c43e4857c87ba4f4a743a6b2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-08 16:35:36 +0000

    M01A01 – Mostrando coisas na tela com Kotlin - Módulo 1

--- M01A01/src/ex001.kt
+++ M01A01/src/ex001.kt
@@ -2,4 +2,13 @@ fun main(args: Array<String>) {
     print ("Olá,")
     print("tudo")
     print("bem!")
+    /*
+         \n   nova linha
+         \t
+         \b
+         \r
+         \\
+         \"
+    *
+    * */
 }
