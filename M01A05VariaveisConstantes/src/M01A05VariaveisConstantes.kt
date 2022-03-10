8529c7609adda391f642563dd9e565854bdab931
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-10 16:46:59 +0000

    M01A05 – Variáveis e Constantes em Kotlin

---
+++ M01A05VariaveisConstantes/src/M01A05VariaveisConstantes.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+
+    var nome:String =  "Gustavo"
+    var idade:Short = 40
+    var peso:Float = 95.8F //Float termina com F
+    var numGrande:Long = 5_000_000L //Long termina co L
+
+    print("O seu nome é ")
+    print(nome)
+    print(", e tenho ")
+    print(idade)
+    print(" anos!")
+
+    /* Regras de nomeação das variaveis em KOTLIN
+
+      Começa com uma letra ou sublinhado
+      Maiúsculas e minúsculas fazem diferença
+      Só usa letras, números e sublinhado
+      Podemos utilizar acentos
+      Não pode   conter espaço
+      Não pode ter símbolos (só sublinhado)
+      não pode ser uma palavra reservada
+
+  * */
+}
