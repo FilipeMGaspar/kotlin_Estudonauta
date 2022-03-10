5699790d2b4876a706e7a22a348c7f3cbd2e6c09
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-10 16:29:25 +0000

    M01A04 – Usando Variáveis com Kotlin

--- M01A04Variaveis/src/M01A04Variaveis.kt
+++ M01A04Variaveis/src/M01A04Variaveis.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    
+    /* Regras de nomeação das variaveis em KOTLIN
+
+        Começa com uma letra ou sublinhado
+        Maiúsculas e minúsculas fazem diferença
+        Só usa letras, números e sublinhado
+        Podemos utilizar acentos
+        Não pode   conter espaço
+        Não pode ter símbolos (só sublinhado)
+        não pode ser uma palavra reservada
+
+    * */
 }
