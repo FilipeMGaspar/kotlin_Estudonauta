bee5212565f0de2002f431f393ee8758831b71f9
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-11 09:04:07 +0000

    M01A07 – Analisando Tipos em Kotlin

--- M01A07AnalisarTipos/src/M01A07AnalisarTipos.kt
+++ M01A07AnalisarTipos/src/M01A07AnalisarTipos.kt
@@ -2,4 +2,8 @@ fun main(args: Array<String>) {
     println("Minimo Byte: " + Int.MIN_VALUE)
     println("Máximo Byte: " + Int.MAX_VALUE)
     println("Tamanho Byte: " + Int.SIZE_BYTES + " bytes")
+
+    println()
+    var nome:String = "Gustavo"
+    println(nome.length)
 }
