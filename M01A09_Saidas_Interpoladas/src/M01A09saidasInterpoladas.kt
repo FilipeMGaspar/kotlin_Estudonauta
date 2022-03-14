150645e81b1ba7970baff7477d6639ab0c876d00
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-14 21:51:05 +0000

    M01A09 – Saídas Interpoladas

--- M01A09_Saidas_Interpoladas/src/M01A09saidasInterpoladas.kt
+++ M01A09_Saidas_Interpoladas/src/M01A09saidasInterpoladas.kt
@@ -1,5 +1,14 @@
 fun main(args: Array<String>) {
-    println("Menu principal")
+//Strings cruas
+    println("""
+       Menu Principal
+         [1] Menu
+         [2] Editar
+         [3] Formatar
+         [4] ....
+    """)
+
+/*  println("Menu principal")
     println("[1]  Novo")
-    println("[2]  Editar")
+    println("[2]  Editar")*/
 }
