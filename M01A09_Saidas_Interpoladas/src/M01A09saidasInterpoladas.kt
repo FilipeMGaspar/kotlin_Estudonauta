308b2325210f009eef2a17c1ed75ed9ec77542f2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-14 22:00:25 +0000

    M01A09 – Saídas Interpoladas

--- M01A09_Saidas_Interpoladas/src/M01A09saidasInterpoladas.kt
+++ M01A09_Saidas_Interpoladas/src/M01A09saidasInterpoladas.kt
@@ -5,6 +5,18 @@ fun main(args: Array<String>) {
 
     println("você nasceu em $nasc e hoje estamos em $ano então você tem ${ano - nasc} anos de idade!")
 
+    println()
+
+    println("""
+       Olá! Tudo bem!
+       você nasceu em $nasc, certo?
+       E atualmente estamos em $ano
+       Logo, fazendo as contas, você
+       faz ${ano - nasc} anos até ao fim do ano de $ano
+       
+         
+    """)
+
 /*
 //Strings cruas
     println("""
