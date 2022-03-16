dcecd285eb20bd5e0f6a8830b331dea41ca435dc
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-16 12:18:49 +0000

    M01A10 – Usando saídas com cores

--- M01A10_ Saidas_a_Cores/src/M01A10 _Saidas_A_cores.kt
+++ M01A10_ Saidas_a_Cores/src/M01A10 _Saidas_A_cores.kt
@@ -1,8 +1,14 @@
 fun main(args: Array<String>) {
     /*
-    *   \u001[00m   RESET
-    *   \u001[30m   PRETO
-    *
+    *   \u001B[00m   RESET
+    *   \u001B[30m   PRETO
+    *   \u001B[31m   VERMELHO
+    *   \u001B[32m   VERDE
+    *   \u001B[33m   AMARELO
+    *   \u001B[34m   AZUL
+    *   \u001B[35m   MAGENTA
+    *   \u001B[36m   CIANO
+    *   \u001B[37m   BRANCO
     *
     * */
 }
