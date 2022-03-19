374dac4d03a077f52fa469821f957bccecfd7ab8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-19 08:10:10 +0000

    M01A12 – Entradas com validação de Tipos

--- M01A12–Entradas_com_Validacao_de_Tipos/src/M01A12_Entradas_cValidacao_de_Tipos.kt
+++ M01A12–Entradas_com_Validacao_de_Tipos/src/M01A12_Entradas_cValidacao_de_Tipos.kt
@@ -2,5 +2,6 @@ fun main(args: Array<String>) {
     print("Qual é o seu nome? ")
     val nome:String = readLine()!! //Utilização do NullCheck !!
     print("Quantos anos você tem. ${nome}")
-    val idade:Int = readLine()!!.toIntOrNull() ?: 0 // Do operador ?:
+    val idade:Int = readLine()!!.toIntOrNull() ?: 0 // Do operador Elvis ?: para o caso do o dado recebido ser diferente do esperado, vai ser atribuido 0
+    print("Finalizando, $nome tem $idade anos!")
 }
