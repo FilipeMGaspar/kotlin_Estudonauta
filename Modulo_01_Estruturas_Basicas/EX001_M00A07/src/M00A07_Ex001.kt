89911106d408ba5b1d2df6eebd6ae0f8f44a9434
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-19 08:28:28 +0000

    M00A07 – Primeiro Programa

--- Modulo_01_Estruturas_Basicas/EX001_M00A07/src/M00A07_Ex001.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-    println("-------------------------------------")
-    println("Olá, Mundo!")
-    println("Ufa! já estou livre da Maldição!")
-    println("-------------------------------------")
-}
