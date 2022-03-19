dc1b6fcfb4acfeddaa8d9db0a94f7c866b98d3d8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-19 08:14:26 +0000

    M01A12 – Entradas com validação de Tipos

--- M01A12–Entradas_com_Validacao_de_Tipos/src/M01A12 _Validacao_de_Tipos-Ex01.kt
+++ M01A12–Entradas_com_Validacao_de_Tipos/src/M01A12 _Validacao_de_Tipos-Ex01.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     print("Digite um valor: ")
     val n1:Int = readLine()!!.toIntOrNull() ?: 0
-    
+    print("Digite outro valor: ")
+    val n2 = readLine()!!.toIntOrNull() ?: 0
+    val soma = n1 + n2 // Soma de dois números
+
+    println()
+    print("A soma de $n1 com $n2  tem o valor de $soma")
 }
