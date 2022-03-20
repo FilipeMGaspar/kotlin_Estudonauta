48d814bd669d0aaa4ba4b476f5a3b1a3b2ea853b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-20 09:32:52 +0000

    M01A13 – Operadores aritméticos do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A13–Operadores_Aritmeticos/src/M01A13–Operadores_Aritmeticos.kt
+++ Modulo_01_Estruturas_Basicas/M01A13–Operadores_Aritmeticos/src/M01A13–Operadores_Aritmeticos.kt
@@ -16,12 +16,22 @@ fun main(args: Array<String>) {
     val n1:Int = 5
     val n2:Int = 2
 
+
+    println()
+    println("$n1 + $n2 = ${n1.plus(n2)} ")
+    println("$n1 - $n2 = ${n1.minus(n2)} ")
+    println("$n1 X $n2 = ${n1.times(n2)} ")
+    println("$n1 / $n2 = ${n1.div(n2)} ")
+    println("$n1 % $n2 = ${n1.mod(n2)} ")
+
+/* OU
     println()
     println("$n1 + $n2 = ${n1 + n2} ")
     println("$n1 - $n2 = ${n1 - n2} ")
     println("$n1 X $n2 = ${n1 * n2} ")
     println("$n1 / $n2 = ${n1.toFloat() / n2} ")
     println("$n1 % $n2 = ${n1 % n2} ")
+*/
 
     /*
     numa divisão
