14cb15ee3c42e45f9c60943237414e370b88d222
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-20 09:22:05 +0000

    M01A13 – Operadores aritméticos do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A13–Operadores_Aritmeticos/src/M01A13–Operadores_Aritmeticos.kt
+++ Modulo_01_Estruturas_Basicas/M01A13–Operadores_Aritmeticos/src/M01A13–Operadores_Aritmeticos.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    
+    /*
+        operadores aritméticos, em Kotlyn antes da compilação são convertidos em métodos
+                                            Métodos
+
+        +   Soma                            .plus()
+        -   Subtração                       .minus()
+        *   Multiplicação
+        /   Divisão
+        %   Resto da divisão inteira
+
+    * */
 }
