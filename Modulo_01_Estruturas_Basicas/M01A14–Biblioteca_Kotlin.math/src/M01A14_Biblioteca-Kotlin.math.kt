197b5a223d3cf00977b3da8197276f9f8ccf9a80
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-23 10:41:15 +0000

    M01A14 – Biblioteca Kotlin.math

--- Modulo_01_Estruturas_Basicas/M01A14–Biblioteca_Kotlin.math/src/M01A14_Biblioteca-Kotlin.math.kt
+++ Modulo_01_Estruturas_Basicas/M01A14–Biblioteca_Kotlin.math/src/M01A14_Biblioteca-Kotlin.math.kt
@@ -16,7 +16,7 @@ fun main(args: Array<String>) {
     println("Potência: ${7f.pow(2)}")
     println("Arredonda acima: ${ceil(2.123f)}")
     println("Arredonda: ${9.3.roundToInt()}")
-    println("Arredonda abaixo: ")
+    println("Arredonda abaixo: ${floor(8.8f)}")
     println("Maior: ")
     println("Menor: ")
     println()
