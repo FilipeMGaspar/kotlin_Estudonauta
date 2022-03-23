ae5993c8000f1d30111b67eafb2f264e4d9ee7bc
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-23 10:54:16 +0000

    M01A15 – Operadores relacionais do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A15–Operadores_relacionais/src/M01A15–Operadores_relacionais.kt
+++ Modulo_01_Estruturas_Basicas/M01A15–Operadores_relacionais/src/M01A15–Operadores_relacionais.kt
@@ -11,8 +11,8 @@ fun main(args: Array<String>) {
     val n1:Int = 5
     val n2:Int = 4
     println()
-    println("$n1 maior que $n2? ")
-    println("$n1 menor que $n2? ")
+    println("$n1 maior que $n2? ${n1 > n2}")
+    println("$n1 menor que $n2? ${n1 < n2}")
     println("$n1 maior ou igual a $n2? ")
     println("$n1 menor ou igual a $n2? ")
     println("$n1 igual a $n2? ")
