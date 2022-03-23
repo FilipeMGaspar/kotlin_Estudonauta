78773cf1ee52e1de1a1cdc6173534106262398f5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-23 10:49:19 +0000

    M01A15 – Operadores relacionais do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A15–Operadores_relacionais/src/M01A15–Operadores_relacionais.kt
+++ Modulo_01_Estruturas_Basicas/M01A15–Operadores_relacionais/src/M01A15–Operadores_relacionais.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
-    
+    /*
+    *   >       Maior
+    *   <       Menor
+    *   =>      Maior ou Igual
+    *   <=      Menor ou Igual
+    *   ==      Igual
+    *   !=      Diferente
+    * */
 }
