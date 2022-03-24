21285880b388c3ba3da79033574812d2ed73d6ce
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-24 16:13:44 +0000

    M01A16 – Operadores de atribuição do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A16–Operadores_de_atribuicao/src/M01A16–Operadores_de-atribuicao.kt
+++ Modulo_01_Estruturas_Basicas/M01A16–Operadores_de_atribuicao/src/M01A16–Operadores_de-atribuicao.kt
@@ -8,10 +8,14 @@
 *
  */
 fun main(args: Array<String>) {
+    var pass:Float = 8.65f
+
+/*
     var tv:Int = 4;
 
     println()
     println("Você tem $tv aparelhos em casa!")
     tv += 3
     println("Neste momento você tem $tv aparelhos!")
+*/
 }
