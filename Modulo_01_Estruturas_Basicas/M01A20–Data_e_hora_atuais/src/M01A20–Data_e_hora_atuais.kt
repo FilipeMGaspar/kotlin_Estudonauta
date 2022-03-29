527fbc466d631a1c268cedaf4c2a21e98052548a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-29 12:41:12 +0100

    M01A20 – Data e hora atuais em Kotlin

--- Modulo_01_Estruturas_Basicas/M01A20–Data_e_hora_atuais/src/M01A20–Data_e_hora_atuais.kt
+++ Modulo_01_Estruturas_Basicas/M01A20–Data_e_hora_atuais/src/M01A20–Data_e_hora_atuais.kt
@@ -1,3 +1,13 @@
+import java.time.LocalDateTime
 fun main(args: Array<String>) {
-    
+    /*
+    *   Data e hora atuais
+    * */
+    var dh = LocalDateTime.now()
+
+    println()
+    println("Dia atual: ${dh.dayOfMonth}")
+    println("Mês Atual: ${dh.monthValue}")
+    println("Ano Atual: ${dh.year}")
+
 }
