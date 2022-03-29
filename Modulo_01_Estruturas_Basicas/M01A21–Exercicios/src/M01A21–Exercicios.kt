d77e7ac595d175fcbc3104d2ca0fb00fd9b3ddf3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-29 13:09:40 +0100

    M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/M01A21–Exercicios.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/M01A21–Exercicios.kt
@@ -1,10 +1,17 @@
 /*
-*   Ecercício 1
-*       
+*   Ecercício 3
 *
+*   -----------------------------
+*        Cadastro Estudonauta
+*   -----------------------------
+*       Nome de Funcionário: João da Silva
+*       Ano de Nascimento: 2003
+*       Salário: R$ 1525
 *
-*
-*
+*   ---------- Ficha Funcional  ----------
+*   Nome:           João da Silva
+*   Nascimento:     2003
+*   Salário:        R$ 1525,00
 *
 *
 * */
