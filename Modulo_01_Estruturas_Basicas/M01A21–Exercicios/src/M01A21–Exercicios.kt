be4bd14f1e723cb2ac99429f8379bc03b3c0ce12
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-29 15:20:07 +0100

    M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/M01A21–Exercicios.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/M01A21–Exercicios.kt
@@ -111,4 +111,20 @@
 *   A diferença foi de 2
 *   ---------------------------------------
 *
+*
+* ============================================
+*     Exercício 10
+*
+*  -----------------------------
+*       Sua idade
+*  -----------------------------
+*   Vou conseguir descobrir a sua idade
+*   em que ano você nasceu? 2000
+*
+*   ---------- Resultados  ----------
+*   Atualmente estamos em 2019
+*   Você, que nasceu em 2000
+*   Completa 19 anos neste ano.
+*   ---------------------------------------
+*
 * */
