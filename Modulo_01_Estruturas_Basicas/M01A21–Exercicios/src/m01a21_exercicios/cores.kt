c011c415f614a06ab715ca522763dd4527ae4851
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-29 13:02:32 +0100

    M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/cores.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/cores.kt
@@ -1,2 +1,12 @@
 package m01a21_exercicios
 
+//Códigos para Cores
+val BRANCO:String = "\u001B[37m "
+val CIANO:String = "\u001B[36m "
+val MAGENTA:String = "\u001B[35m "
+val AZUL:String = "\u001B[34m "
+val VERDE:String = "\u001B[32m"
+val VERMELHO:String = "\u001B[31m"
+val PRETO:String = "\u001B[30m"
+val RESET:String = "\u001B[00m"
+val AMARELO:String = "\u001B[33m"
