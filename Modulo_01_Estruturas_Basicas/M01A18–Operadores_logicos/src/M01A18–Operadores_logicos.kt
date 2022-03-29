06252f536aabe310bf8f41e68c02d5bbd8a474c3
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-29 10:49:25 +0100

    M01A18 – Operadores lógicos do Kotlin

--- Modulo_01_Estruturas_Basicas/M01A18–Operadores_logicos/src/M01A18–Operadores_logicos.kt
+++ Modulo_01_Estruturas_Basicas/M01A18–Operadores_logicos/src/M01A18–Operadores_logicos.kt
@@ -24,7 +24,7 @@ fun main(args: Array<String>) {
     *       Aritméticos
     *       Relacionais
     *       Lógicos
-    *   
+    *               !      &&      ||
 
 *     Conjunção     AND (E)
 *___________________________
