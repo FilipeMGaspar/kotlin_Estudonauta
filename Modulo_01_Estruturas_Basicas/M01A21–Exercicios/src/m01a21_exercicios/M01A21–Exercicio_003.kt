5394306490ed4ebe45529e2cd55a2333faef8253
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-31 16:11:20 +0100

    Exercício 3 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_003.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_003.kt
@@ -1,2 +1,22 @@
 package m01a21_exercicios
 
+fun main(args: Array<String>) {
+    println("${CIANO}Olá${RESET} Mundo")
+}
+
+/*
+*   Ecercício 3
+*
+*   -----------------------------
+*        Cadastro Estudonauta
+*   -----------------------------
+*       Nome de Funcionário: João da Silva
+*       Ano de Nascimento: 2003
+*       Salário: R$ 1525
+*
+*   ---------- Ficha Funcional  ----------
+*   Nome:           João da Silva
+*   Nascimento:     2003
+*   Salário:        R$ 1525,00
+*   ---------------------------------------
+ */
