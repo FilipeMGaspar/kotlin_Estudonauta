3a809199fa36627e2f16bf275096293e1d23178e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-03-31 17:01:40 +0100

    Exercício 4 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
@@ -1,2 +1,24 @@
 package m01a21_exercicios
 
+fun main(args: Array<String>) {
+
+}
+
+/*
+*     Exercício 4
+*
+*  -----------------------------
+*        Dois Valores
+*   -----------------------------
+*
+*       Valor 1: 7
+*       Valor 2: 3
+*
+*   ---------- Ficha Funcional  ----------
+*   Soma:           7 + 3 = 10
+*   Subtração:      7 - 3 = 4
+*   Multiplicação:  7 * 3 = 21
+*   Divisão:        7 / 3 = 2.3333
+*   Módulo:         7 % 3 = 1
+*   ---------------------------------------
+* */
