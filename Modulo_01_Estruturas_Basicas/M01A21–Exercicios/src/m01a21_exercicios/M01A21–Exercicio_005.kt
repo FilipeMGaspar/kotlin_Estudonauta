0c938cab47dc67cfca738bbc07f3d9c451047666
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-01 11:17:25 +0100

    Exercício 5 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_005.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_005.kt
@@ -1,7 +1,19 @@
 package m01a21_exercicios
 
 fun main(args: Array<String>) {
-    
+    var nota1:Float
+    var nota2:Float
+
+    println("${VERMELHO}--------------------------------------")
+    println("\tNOTAS ESTUDONAUTA")
+    println("--------------------------------------${RESET}")
+    print("Nota 1: ")
+    nota1 = readLine()!!.toFloatOrNull()?:0f
+    print("Nota 2: ")
+    nota2 = readLine()!!.toFloatOrNull()?:0f
+    println("${AMARELO}------------ RESULTADOS ----------------")
+    println("As notas do aluno foram ${RESET} ${MAGENTA} ${nota1} ${RESET} ${AMARELO} e ${RESET} ${MAGENTA} ${nota2} ${RESET}")
+    println("${AMARELO}----------------------------------------${RESET}")
 }
 
 /*
