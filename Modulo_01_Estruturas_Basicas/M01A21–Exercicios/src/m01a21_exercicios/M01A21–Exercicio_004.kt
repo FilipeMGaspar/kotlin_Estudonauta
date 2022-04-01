bc5918725c1472ff5869576afa1f42502dc3032a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-01 11:00:23 +0100

    Exercício 4 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
@@ -15,11 +15,11 @@ fun main(args: Array<String>) {
     val2 = readLine()!!.toIntOrNull()?:0
 
     println("${AMARELO}------------ RESULTADOS ------------")
-    println("Soma: ${RESET} ${AZUL} \t\t ${val1} + ${val2} = ${RESET} ${MAGENTA} ${val1 + val2} ${RESET}")
-    println("${AMARELO}Subtração: ${RESET} ${AZUL} \t ${val1} - ${val2} = ${RESET} ${MAGENTA} ${val1 - val2} ${RESET}")
+    println("Soma: ${RESET} ${AZUL} \t\t\t ${val1} + ${val2} = ${RESET} ${MAGENTA} ${val1 + val2} ${RESET}")
+    println("${AMARELO}Subtração: ${RESET} ${AZUL} \t\t ${val1} - ${val2} = ${RESET} ${MAGENTA} ${val1 - val2} ${RESET}")
     println("${AMARELO}Multiplicação: ${RESET} ${AZUL}  ${val1} * ${val2} = ${RESET} ${MAGENTA} ${val1 * val2} ${RESET}")
-    println("${AMARELO}Divisão: ${RESET} ${AZUL} \t ${val1} / ${val2} = ${RESET} ${MAGENTA} ${val1.toFloat() / val2.toFloat()} ${RESET}")
-    println("${AMARELO}Módulo: ${RESET} ${AZUL} \t\t ${val1} % ${val2} = ${RESET} ${MAGENTA} ${val1 % val2} ${RESET}")
+    println("${AMARELO}Divisão: ${RESET} ${AZUL} \t\t ${val1} / ${val2} = ${RESET} ${MAGENTA} ${val1.toFloat() / val2.toFloat()} ${RESET}")
+    println("${AMARELO}Módulo: ${RESET} ${AZUL} \t\t\t ${val1} % ${val2} = ${RESET} ${MAGENTA} ${val1 % val2} ${RESET}")
     println("${AMARELO}------------------------------------${RESET}")
 }
 
