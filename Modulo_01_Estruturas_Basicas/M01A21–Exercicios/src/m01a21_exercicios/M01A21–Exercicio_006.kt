30f97a20b44b620fa2f253de2e029e579a142d95
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-01 11:34:09 +0100

    Exercício 6 - M01A21 – Exercícios

---
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_006.kt
@@ -0,0 +1,17 @@
+package m01a21_exercicios
+
+/*
+  Exercício 6
+*
+*  -----------------------------
+*       Cálculo de Potência
+*  -----------------------------
+*
+*       Base: 5
+*       Expoente: 2
+*
+*   ---------- Resultados  ----------
+*   Calculando 5 elevado a 2
+*   O resultado é 25.0
+*   ---------------------------------------
+* */
