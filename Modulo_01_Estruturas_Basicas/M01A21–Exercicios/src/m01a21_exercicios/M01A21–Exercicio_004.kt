2b1d020945eac36c5e943bf7a66feb1e0d16697a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-01 10:50:29 +0100

    Exercício 4 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_004.kt
@@ -1,11 +1,22 @@
 package m01a21_exercicios
 
 fun main(args: Array<String>) {
+
+    var val1:Int
+    var val2:Int
+
     println()
     println("${VERMELHO}-----------------------------------------")
     println("\t\t\tDOIS VALORES")
     println("-----------------------------------------${RESET}")
+    print("Valor 1: ")
+    val1= readLine()!!.toIntOrNull()?:0
+    print("Valor 2: ")
+    val2 = readLine()!!.toIntOrNull()?:0
 
+    println("${AMARELO}------------ RESULTADOS ------------")
+    println("Soma: ${RESET} ${AZUL} ${val1} + ${val2} = ${RESET} ${VERDE} ${val1 + val2} ${RESET}")
+    println("${AMARELO}------------------------------------${RESET}")
 }
 
 /*
