c386c444629ca15786973b28659c536770163fa8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-01 11:07:08 +0100

    Exercício 5 - M01A21 – Exercícios

---
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_005.kt
@@ -0,0 +1,18 @@
+package m01a21_exercicios
+
+
+/*
+*     Exercício 5
+*
+*  -----------------------------
+*        Notas Estudonauta
+*   -----------------------------
+*
+*       Nota 1: 7.5
+*       Nota 2: 2
+*
+*   ---------- Resultados  ----------
+*   As notas do aluno foram 7.5 e 2.0
+*   A média final foi 4.75
+*   ---------------------------------------
+ */
