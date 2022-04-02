b05d0552654179be5a58353635130e54c1aed266
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-02 19:22:24 +0100

    Exercício 7 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_007.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_007.kt
@@ -1,2 +1,19 @@
 package m01a21_exercicios
 
+/*
+*     Exercício 7
+*
+*  -----------------------------
+*       Reajuste Salarial
+*  -----------------------------
+*
+*       Salário: R$  1000
+*       Reajuste (%): 10
+*
+*   ---------- Reajustando  ----------
+*   O salário anterior era R$ 1000,00
+*   O aumento será de      10.0%
+*   E passará a ganhar     R$ 1100,00
+*   ---------------------------------------
+*
+* */
