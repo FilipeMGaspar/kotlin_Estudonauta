f0927182cea2887d4d35e176cc1921b82a04c8b6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-03 20:07:21 +0100

    Exercício 8 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_008.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_008.kt
@@ -1,2 +1,20 @@
 package m01a21_exercicios
 
+/**
+ * 
+ *     Exercício 8         Somente 50, 10, 2, 1
+ *
+ *  -----------------------------
+ *       Banco Estudonauta
+ *  -----------------------------
+ *
+ *       Que valor você quer sacar? R$ 1235
+ *
+ *   ---------- saque realizado  ----------
+ *   R$50,00      =  24  cédula(s)
+ *   R$10,00      =  3   cédula(s)
+ *   R$2,00       =  2   cédula(s)
+ *   R$1,00       =  1   moeda(s)
+ *   ---------------------------------------
+ *
+ **/
