7655f45a6cf6bbcb7e20a1da8bf6d3b3653fd9f5
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-04 09:39:43 +0100

    Exercício 8 - M01A21 – Exercícios

--- Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_009.kt
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_009.kt
@@ -1,2 +1,17 @@
 package m01a21_exercicios
 
+/*
+*     Exercício 9
+*
+*  -----------------------------
+*       Tente Adivinhar
+*  -----------------------------
+*   Vou "pensar" em um número entre 0 e 5
+*   Seu palpite: 1
+*
+*   ---------- Resultados  ----------
+*   você disse que seria o valor 1
+*   Eu pensei no número 3
+*   A diferença foi de 2
+*   ---------------------------------------
+* */
