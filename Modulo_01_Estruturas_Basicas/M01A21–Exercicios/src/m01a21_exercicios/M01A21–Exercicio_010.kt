4b97a12c2fe87b34e9acedc0f565e770168b530e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2022-04-05 09:50:09 +0100

    Exercício 10 - M01A21 – Exercícios

---
+++ Modulo_01_Estruturas_Basicas/M01A21–Exercicios/src/m01a21_exercicios/M01A21–Exercicio_010.kt
@@ -0,0 +1,19 @@
+package m01a21_exercicios
+
+/*
+*
+*     Exercício 10
+*
+*  -----------------------------
+*       Sua idade
+*  -----------------------------
+*   Vou conseguir descobrir a sua idade
+*   em que ano você nasceu? 2000
+*
+*   ---------- Resultados  ----------
+*   Atualmente estamos em 2019
+*   Você, que nasceu em 2000
+*   Completa 19 anos neste ano.
+*   ---------------------------------------
+* 
+* */
